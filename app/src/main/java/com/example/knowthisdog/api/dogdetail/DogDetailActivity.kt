699a6e251ab4df7cdf4aa2613b22859081da9a13
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-06-07 00:40:34 -0300

    feat: add Detail activity

---
+++ app/src/main/java/com/example/knowthisdog/api/dogdetail/DogDetailActivity.kt
@@ -0,0 +1,35 @@
+package com.example.knowthisdog.api.dogdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.knowthisdog.Dog
+import com.example.knowthisdog.R
+import com.example.knowthisdog.databinding.ActivityDogDetailBinding
+
+class DogDetailActivity : AppCompatActivity() {
+
+    companion object{
+      const val DOG_KEY = "dog"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDogDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val dog = intent?.extras?.getParcelable<Dog>(DOG_KEY)
+
+        if(dog == null) {
+            Toast.makeText(this, "Dog not found", Toast.LENGTH_LONG)
+            finish()
+            return
+        }
+
+        binding.dogIndex.text = getString(R.string.dog_index_format, dog.index)
+        binding.lifeExpectancy.text = getString(R.string.dog_life_expectancy, dog.lifeExpectancy)
+        binding.dog = dog
+    }
+}
