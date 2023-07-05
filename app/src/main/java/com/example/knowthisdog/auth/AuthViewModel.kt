3aeffe7ba4b7dd036a82655c4905b996d74f1d09
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-07-05 19:51:47 -0300

    Feat: add ViewModel

---
+++ app/src/main/java/com/example/knowthisdog/auth/AuthViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.knowthisdog.auth
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+    fun signUp(email: String, password: String,
+     passwordConfirmation: String) {
+
+    }
+}
