4148066e4a8df49445bd6752fc2e3b47e21cf409
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-07-06 22:47:30 -0300

    feat: add fun login

--- app/src/main/java/com/example/knowthisdog/auth/AuthRepository.kt
+++ app/src/main/java/com/example/knowthisdog/auth/AuthRepository.kt
@@ -2,14 +2,31 @@ package com.example.knowthisdog.auth
 
 import com.example.knowthisdog.api.ApiResponseStatus
 import com.example.knowthisdog.api.DogsApi
-import com.example.knowthisdog.api.dto.DogDTOMapper
+import com.example.knowthisdog.api.dto.LoginDTO
 import com.example.knowthisdog.api.dto.SignUpDTO
 import com.example.knowthisdog.api.dto.UserDTOMapper
 import com.example.knowthisdog.api.makeNetworkCall
 import com.example.knowthisdog.model.User
 
 class AuthRepository {
-    suspend fun signUp(email: String, password: String,
+
+
+
+    suspend fun login(email: String, password: String): ApiResponseStatus<User> = makeNetworkCall {
+        val loginDTO = LoginDTO(email, password)
+        val loginResponse = DogsApi.retrofitService.login(loginDTO)
+
+
+        if( loginResponse.isSucess) {
+            throw Exception(loginResponse.message)
+        }
+
+        val userDTO = loginResponse.data.user
+        val userDTOMapper = UserDTOMapper()
+        userDTOMapper.fromUserDTOToUserDomain(userDTO)
+    }
+
+suspend fun signUp(email: String, password: String,
                        passwordConfirmation: String): ApiResponseStatus<User> = makeNetworkCall {
         val signUpDTO = SignUpDTO(email, password, passwordConfirmation)
         val signUpResponse = DogsApi.retrofitService.signUp(signUpDTO)
