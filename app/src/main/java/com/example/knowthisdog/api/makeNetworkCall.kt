3148e07e96e6cff6536192abe1f52cc48c947e7f
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-07-06 22:48:20 -0300

    feat: add a Exception for code HTTP 401

--- app/src/main/java/com/example/knowthisdog/api/makeNetworkCall.kt
+++ app/src/main/java/com/example/knowthisdog/api/makeNetworkCall.kt
@@ -3,16 +3,25 @@ package com.example.knowthisdog.api
 import com.example.knowthisdog.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import java.net.UnknownHostException
 
+
+private  const val UNAUTHORIZED_ERROR_CODE = 401
 suspend fun <T> makeNetworkCall(
     call: suspend () -> T
-): ApiResponseStatus<T> {
-    return withContext(Dispatchers.IO) {
+): ApiResponseStatus<T> = withContext(Dispatchers.IO) {
         try {
             ApiResponseStatus.Success(call())
         } catch (e: UnknownHostException) {
             ApiResponseStatus.Error(R.string.unknown_host_exception_error)
+        } catch (e: HttpException){
+          val errorMessage =  if(e.code() == UNAUTHORIZED_ERROR_CODE){
+          R.string.wrong_user_or_password
+            } else {
+              R.string.unknown_error
+          }
+            ApiResponseStatus.Error(errorMessage)
         } catch (e: Exception){
            val errorMessage = when(e.message) {
                 "sign_up_error" -> R.string.error_sign_up
@@ -23,4 +32,3 @@ suspend fun <T> makeNetworkCall(
             ApiResponseStatus.Error(errorMessage)
         }
     }
-}
