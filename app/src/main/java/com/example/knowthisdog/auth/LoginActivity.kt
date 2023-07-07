9cb5d2432127213b93db2046ea7747eaf09caf26
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-07-06 22:50:51 -0300

    feat: add validate fields to login

--- app/src/main/java/com/example/knowthisdog/auth/LoginActivity.kt
+++ app/src/main/java/com/example/knowthisdog/auth/LoginActivity.kt
@@ -1,26 +1,70 @@
 package com.example.knowthisdog.auth
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.findNavController
+import com.example.knowthisdog.MainActivity
 import com.example.knowthisdog.R
+import com.example.knowthisdog.api.ApiResponseStatus
 import com.example.knowthisdog.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions, SignUpFragment.SignUpFragmentActions {
 
 
-
+ private val viewModel : AuthViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel.status.observe(this) { status ->
+            when (status) {
+                is ApiResponseStatus.Error -> {
+                    binding.loadingWheel.visibility = View.GONE
+                    showErrorDialog(status.messageId)
+
+                }
+                is ApiResponseStatus.Loading -> binding.loadingWheel.visibility = View.VISIBLE
+                is ApiResponseStatus.Success -> binding.loadingWheel.visibility = View.GONE
+            }
+        }
+        viewModel.user.observe(this){
+           user ->
+if(user != null){
+    startMainActivity()
+}
+
+        }
     }
 
+    private fun startMainActivity(){
+startActivity(Intent(this,MainActivity::class.java))
+    }
+    private fun showErrorDialog(messageId: Int){
+        AlertDialog.Builder(this)
+            .setTitle(R.string.there_was_an_error)
+            .setMessage(messageId)
+            .setPositiveButton(android.R.string.ok) { _, _ -> }
+            .create()
+            .show()
+            }
+
+
     override fun onRegisterButtonClick() {
-        findNavController(R.id.auth_nav_graph)
+        findNavController(R.id.nav_host_fragment)
             .navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment())
+    }
+
+    override fun onLoginFieldsValidated(email: String, password: String) {
+        viewModel.login(email, password)
     }
 
     override fun onSignUpFieldsValidated(
@@ -28,6 +72,6 @@ class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions, S
         password: String,
         passwordConfirmation: String
     ) {
-        TODO("Not yet implemented")
+        viewModel.signUp(email, password, passwordConfirmation)
     }
 }
--- app/src/main/java/com/example/knowthisdog/auth/LoginFragment.kt
+++ app/src/main/java/com/example/knowthisdog/auth/LoginFragment.kt
@@ -2,28 +2,32 @@ package com.example.knowthisdog.auth
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.knowthisdog.R
+import com.example.knowthisdog.databinding.FragmentLoginBinding
+import com.example.knowthisdog.isValidEmail
 
 
 class LoginFragment : Fragment() {
 
     interface  LoginFragmentActions{
         fun  onRegisterButtonClick()
+        fun  onLoginFieldsValidated(email: String, password: String)
     }
 
     private lateinit var loginFragmentActions: LoginFragmentActions
-
+    private lateinit var  binding: FragmentLoginBinding
 
     override fun onAttach(context: Context){
         super.onAttach(context)
         loginFragmentActions = try {
             context as LoginFragmentActions
         } catch (e: ClassCastException) {
-            throw ClassCastException("#$context must implement LoginFragemetActions")
+            throw ClassCastException("$context must implement LoginFragmentActions")
         }
     }
 
@@ -31,8 +35,37 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater)
+        binding.loginRegisterButton.setOnClickListener{
+            loginFragmentActions.onRegisterButtonClick()
+        }
+        binding.loginButton.setOnClickListener{
+            validateFields()
+        }
+        return binding.root
+    }
+
+    private fun validateFields() {
+        binding.emailInput.error = ""
+        binding.passwordInput.error = ""
+
+        val email = binding.emailEdit.text.toString()
+
+        if (!isValidEmail(email)){
+            binding.emailInput.error = getString(R.string.email_not_valid)
+            return
+        }
+        val password = binding.passwordEdit.text.toString()
+
+        if (password.isEmpty()){
+            binding.passwordInput.error =getString(R.string.pasword_empty)
+            return
+        }
+
+
+
+        loginFragmentActions.onLoginFieldsValidated(email, password)
     }
 
 
