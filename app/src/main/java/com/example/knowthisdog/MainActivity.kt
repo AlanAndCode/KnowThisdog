45dceaacbb89f1950075b805be078d1f0943501a
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-07-08 17:44:45 -0300

    Feat: add call settings and list

--- app/src/main/java/com/example/knowthisdog/MainActivity.kt
+++ app/src/main/java/com/example/knowthisdog/MainActivity.kt
@@ -4,19 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.knowthisdog.auth.LoginActivity
-import com.example.knowthisdog.model.User
+import com.example.knowthisdog.auth.model.User
+import com.example.knowthisdog.databinding.ActivityMainBinding
+import com.example.knowthisdog.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val user = User.getLoggedInUser(this)
         if (user == null){
             openLoginActivity()
             return
         }
+
+        binding.settingsFab.setOnClickListener {
+            openSettingsActivity()
+        }
+
+
+        binding.dogListFab.setOnClickListener {
+            openListActivity()
+        }
+    }
+
+    private fun openListActivity() {
+        startActivity(Intent(this, SettingsActivity::class.java))
+    }
+
+    private fun openSettingsActivity() {
+        startActivity(Intent(this, SettingsActivity::class.java))
     }
 
     private fun openLoginActivity() {
