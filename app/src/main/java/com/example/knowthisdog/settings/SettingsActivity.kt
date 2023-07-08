0c857c3ef34706532894223b4a563f43f98ad713
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-07-08 17:40:21 -0300

    feat: ADD setings activity

---
+++ app/src/main/java/com/example/knowthisdog/settings/SettingsActivity.kt
@@ -0,0 +1,30 @@
+package com.example.knowthisdog.settings
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.knowthisdog.R
+import com.example.knowthisdog.auth.LoginActivity
+import com.example.knowthisdog.auth.model.User
+import com.example.knowthisdog.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logoutButton.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout() {
+User.logout(this)
+        val intent = Intent(this, LoginActivity::class.java)
+    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
+
+}
